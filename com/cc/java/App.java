df65287311aa29fec87263585218c8d43618f6ea
eva <e.moritz.comcave@gmail.com>
2023-05-23 17:02:32 +0200

    test2

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -5,23 +5,18 @@ public class App {
     public static void main(String[] args) {
 
        Konto konto1 = new Konto(10000, 2);
-
-        output(Integer.toString(konto1.getKontostandAnfang()));
-
-        Konto konto2 = new Konto(10000, 3);
-
-        output(Integer.toString(konto2.getKontostandAnfang()));
-
-        Konto konto3 = new Konto(10000, 10);
-
-        output(Integer.toString(konto3.getKontostandAnfang()));
+       Konto konto2 = new Konto(10000, 3);
+       Konto konto3 = new Konto(10000, 10);
+        
+       
+       output(Integer.toString(konto1.getKontostandAnfang()));
+       output(Integer.toString(konto2.getKontostandAnfang()));
+       output(Integer.toString(konto3.getKontostandAnfang()));
 
         output ("------------------");
 
         output(Integer.toString(konto1.getKontostandAnfang() * konto1.getMultiplikatorGeld()));
-
         output(Integer.toString(konto2.getKontostandAnfang() * konto2.getMultiplikatorGeld()));
-
         output(Integer.toString(konto3.getKontostandAnfang() * konto3.getMultiplikatorGeld()));
  }
 
